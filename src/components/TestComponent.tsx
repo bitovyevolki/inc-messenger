f1748b7cf38814232b0c54d5d5fc4b761ab72913
VladimirS <v.a.samarin@yandex.ru>
2024-10-11 18:05:00 +0300

    init

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,7 @@
+const TestComponent = () => {
+	return ( <div style={{padding: '50px', border: '2px solid red', color: 'red', width: 'fit-content', background: 'red'}}>
+		Hello world
+	</div> );
+}
+
+export default TestComponent
